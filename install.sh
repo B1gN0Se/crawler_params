d9c647192e28ebc64b87c2d7721ced75e16774b2
Thiago Pereira <86577632+thpereira1@users.noreply.github.com>
2024-03-16 17:51:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ mv /root/go/bin/gau /usr/bin/
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 mv /root/go/bin/httpx /root/go/bin/httpx_bug
 mv /root/go/bin/httpx_bug /usr/bin/
-go install github.com/projectdiscovery/katana/cmd/katana@latest
-mv /root/go/bin/katana /usr/bin/
+go install github.com/hakluke/hakrawler@latest
+mv /root/go/bin/hakrawler /usr/bin/
 pip3 install uro
