0ac8a45dce435d6d93b80e79b931fcc853ef0f4d
B1gN0Se <86577632+B1gN0Se@users.noreply.github.com>
2024-09-22 00:21:34 -0300

    Update crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -1,4 +1,4 @@
 for y in $(cat subdomains_final); do gau "$y" | grep "=" | sort | uniq | httpx_bug -silent >> urls; done;
 for y in $(cat subdomains_final); do cat subdomains_final | hakrawler -t 16 -d 100 | sort | uniq | grep "$y" | grep "=" >> urls; done;
-cat urls | sort | uniq | uro >> urls_final
+cat urls | grep "?" | sort | uniq | uro >> urls_final
 rm urls
